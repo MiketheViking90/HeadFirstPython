052b65c8698982e78b0b8d1c71fb3b23ad67b157
Jim Wen <siliwen@amazon.com>
2019-09-13 15:26:34 -0700

    Modules intro

--- ch01/odd.py
+++ ch01/odd.py
@@ -5,3 +5,24 @@ if (this_min % 2 != 0):
     print("this min seems odd")
 else:
     print("not an odd min")
+
+from os import getcwd
+
+dir = getcwd()
+print(dir)
+
+import sys
+platform = sys.platform
+print(platform)
+print(sys.version)
+
+import datetime
+today = datetime.date.today()
+print(today)
+
+print(today.day)
+print(today.month)
+print(today.year)
+
+iso = datetime.date.isoformat(today)
+print(iso)
