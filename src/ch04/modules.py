d4b29c0d219df951e945fef6b73b3f3a11d03d0b
Sili Wen <sili@SpaceGrey.local>
2019-10-01 10:57:24 -0700

    Create a module using setuptools and pip

---
+++ src/ch04/modules.py
@@ -0,0 +1,19 @@
+from src.mymodules.vsearch import search_for_letters
+
+search_for_letters("djfieifjef", set("aeiou"))
+
+
+def test(v):
+    v = v+5
+
+
+def test_dict(dict):
+    dict['a'] = 12
+
+a = 7
+test(a)
+print(a)
+
+d = { 'b' : 15 }
+test_dict(d)
+print(d)
---
+++ src/mymodules/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='vsearch',
+    version='1.0',
+    description='Search Tools',
+    author='siliwen',
+    author_email='siliwen@gmail.com',
+    py_modules=['vsearch']
+)
