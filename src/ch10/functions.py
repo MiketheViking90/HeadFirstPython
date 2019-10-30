9ce04d6b27f4c6a7dadec922a1d6e6e8d3406f53
Jim Wen <jimwen@lyft.com>
2019-10-29 17:28:06 -0700

    Use args and kwargs together

--- src/ch10/functions.py
+++ src/ch10/functions.py
@@ -8,8 +8,45 @@ def outer():
     print('outer invoked, return inner()')
     return inner
 
+def myfunc(*args):
+    for arg in args:
+        print(arg, end=' ')
+    if args:
+        print()
+
+def myfunc2(**kwargs):
+    for k, v in kwargs.items():
+        print(k, v, sep='->', end=' ')
+    if kwargs:
+        print()
+
+def myfunc3(*args, **kwargs):
+    if args:
+        for arg in args:
+            print(arg, end=' ')
+        print()
+    if kwargs:
+        for k, v in kwargs.items():
+            print(k, v, sep='->', end=' ')
+        print()
+
 apply(print, "hello!")
 print(apply(id, "hello!"))
 print(apply(type, "hello!"))
 
 outer()
+
+myfunc(1,2,3,4)
+
+vals = (1,2,3,4,5)
+myfunc(vals)
+myfunc(*vals)
+
+myfunc2(a='1', b='2')
+dict = {
+    'a': 1,
+    'b': 12,
+    'x': 34
+}
+myfunc2(**dict)
+myfunc3(*vals, **dict)
